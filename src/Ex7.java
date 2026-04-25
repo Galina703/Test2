a819f0fc4e3e4d405dcf7c6bc122bbd434383eb5
Neo-PC <galinabunu26@gmail.com>
2026-04-25 13:08:17 +0200

    Ex7

---
+++ src/Ex7.java
@@ -0,0 +1,7 @@
+public class Ex7 {
+    public static void main(String[] args) {
+        String s = "    Programare Java    ";
+        String rezultat = s.trim();
+        System.out.println(" Rezultat : '" +rezultat + "'");
+    }
+}
