6c7d99040ed551b3bcbdcfa3d56557fcec90e1ca
Neo-PC <galinabunu26@gmail.com>
2026-04-25 12:31:29 +0200

    Ex4

---
+++ src/Exercitiu4.java
@@ -0,0 +1,11 @@
+public class Exercitiu4 {
+    public static void main(String[] args) {
+        System.out.println("Numerele intre 20 si 40 sunt : ");
+        for (int i = 20; i <= 40; i++) {
+            System.out.print(i + " ");
+
+
+        }
+    }
+}
+
