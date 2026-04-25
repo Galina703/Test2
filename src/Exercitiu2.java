aa129b145d4e7cd59c0a2c7f8b9480267a0256f8
Neo-PC <galinabunu26@gmail.com>
2026-04-25 12:24:13 +0200

    Ex2

--- src/Exercitiu2.java
+++ src/Exercitiu2.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Exercitiu2 {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduceti un numar: ");
+        double nr = sc.nextDouble();
+
+        if (nr >= 0) {
+        System.out.println("Pozitiv.");
+
+    } else {
+            System.out.println("Negativ.");
+        }
+
+
+
+
+  }
+  }
+
+
+
+
+
