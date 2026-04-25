5d661a4d5838d8d2a3ef5ed327bac9b394b586a2
Neo-PC <galinabunu26@gmail.com>
2026-04-25 12:51:42 +0200

    Ex5

--- src/Exercitiu5.java
+++ src/Exercitiu5.java
@@ -1,2 +1,10 @@
 public class Exercitiu5 {
+    public static void main(String[] args) {
+        int [] numere = { 10, 20, 30, 40, 50};
+        int suma = 0;
+        for (int n : numere) {
+            suma += n;
+        }
+        System.out.println("Suma elementelor : " + suma);
+    }
 }
