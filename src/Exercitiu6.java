132b13f220545e208102384f9e73547b7797d990
Neo-PC <galinabunu26@gmail.com>
2026-04-25 13:00:57 +0200

    Ex6

---
+++ src/Exercitiu6.java
@@ -0,0 +1,11 @@
+public class Exercitiu6 {
+    public static void main(String[] args) {
+        String propozitie = " Motanul maninca multi soareci mici.";
+        int prima = propozitie.indexOf('m');
+        int ultima = propozitie.lastIndexOf('m');
+
+        System.out.println("Prima aparitie a lui 'm': " + prima);
+        System.out.println("Ultima aparitie a lui 'm': " + ultima);
+
+    }
+}
