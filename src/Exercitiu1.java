33d530b468273fa0c397769ecfa062bc15926978
Neo-PC <galinabunu26@gmail.com>
2026-04-25 11:59:39 +0200

    Ex 1

--- src/Exercitiu1.java
+++ src/Exercitiu1.java
@@ -1,5 +1,16 @@
 public class Exercitiu1 {
     public static void main(String[] args) {
+        byte b = 20;
+        short s = b; // byte in short
+        System.out.println("Short: " + s);
+
+        int i = 80;
+        double d = i; //int in double
+        System.out.println("Double: " + d);
+
+        long l = 1000;
+        int i2 = (int) l; //long in int
+        System.out.println("Int din long: " +i2);
 
     }
 
